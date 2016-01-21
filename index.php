52f98d6591320abf5c8e03d18b42ea37d798a8ae
Hoboken Jawbreaker <hoboken.jawbreaker@gmail.com>
2016-01-21 20:14:39 +0000

    Experiment with chart options

--- index.php
+++ index.php
@@ -5,6 +5,12 @@
   <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js">
   </script>
   <script type="text/javascript" src="js/ganttdemo.js"></script>
+  <style>
+    html {
+      font-family: helvetica, arial, sans-serif;
+      font-size: 16px;
+    }
+  </style>
 </head>
 
 <body>
